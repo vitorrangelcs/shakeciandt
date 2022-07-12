fea11b23117fe4da5d6a9eaedea08e48cf9b394c
Rebecabfv <re.bfviana@gmail.com>
2022-07-12 12:34:38 -0300

    Fix errors

--- src/main/java/produto/TipoTamanho.java
+++ src/main/java/produto/TipoTamanho.java
@@ -1,8 +1,12 @@
 package produto;
 
 public enum TipoTamanho {
+    P, M, G;
     //IMPLEMENTE A LOGICA DO ENUM
     //TODO
     public final double multiplicador;
 
+    TipoTamanho() {
+        multiplicador = 0;
+    }
 }

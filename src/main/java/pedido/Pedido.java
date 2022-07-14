c52dfb4665973154a679ef0c881738356a6cf601
Jefferson Rhudson <jrhudson@ciandt.com>
2022-07-14 15:00:00 -0300

    1.3v: otimização da classe Pedido.java
    - Substituição de um arraylist por um linkedhashmap

--- src/main/java/pedido/Pedido.java
+++ src/main/java/pedido/Pedido.java
@@ -1,23 +1,31 @@
 package pedido;
 
 import exception.ItemNaoEncontradoException;
+import produto.Shake;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Pedido{
 
     private int id;
-    private List<ItemPedido> itens;
+    private Map<Shake, ItemPedido> itens;
     private Cliente cliente;
 
-    public Pedido(int id, List<ItemPedido> itens,Cliente cliente){
+    public Pedido(int id, Collection<ItemPedido> itens, Cliente cliente){
         this.id = id;
-        this.itens=itens;
+        this.itens=new LinkedHashMap<>(itens.stream()
+                                            .collect(Collectors.toMap(ItemPedido::getShake,
+                                                                    itemPedido -> itemPedido)));
         this.cliente=cliente;
     }
 
     public List<ItemPedido> getItens() {
-        return itens;
+        return new ArrayList<>(itens.values());
     }
 
     public int getId(){
@@ -29,7 +37,8 @@ public class Pedido{
     }
 
     public double calcularTotal(Cardapio cardapio){
-        return itens.stream()
+        return itens.values()
+                .stream()
                                         .map(pedido -> {
                                                         final var shake = pedido.getShake();
                                                         final var adicionais = shake.getAdicionais();
@@ -45,32 +54,33 @@ public class Pedido{
     }
 
     public void adicionarItemPedido(ItemPedido itemPedidoAdicionado){
-        final var itemIdx = itens.indexOf(itemPedidoAdicionado);
-        if (itemIdx != -1){
-            final var item = itens.get(itemIdx);
-            final var quantidade = item.getQuantidade();
-            item.setQuantidade(quantidade+itemPedidoAdicionado.getQuantidade());
-            itens.set(itemIdx, item);
+        final var shake = itemPedidoAdicionado.getShake();
+        final var itemExistente = itens.get(shake);
+
+        if (itemExistente != null){
+            final var quantidadeFinal = itemExistente.getQuantidade() + itemPedidoAdicionado.getQuantidade();
+            itemExistente.setQuantidade(quantidadeFinal);
+            itens.put(shake, itemExistente);
         }else {
-            itens.add(itemPedidoAdicionado);
+            itens.put(shake, itemPedidoAdicionado);
         }
     }
 
-    public boolean removeItemPedido(ItemPedido itemPedidoRemovido) {
-        try {
-            final var itemIdx = itens.indexOf(itemPedidoRemovido);
-            final var item = itens.get(itemIdx);
-            final var quantidade = item.getQuantidade() - 1;
-            item.setQuantidade(quantidade);
-            if (quantidade > 0){
-                itens.set(itemIdx, item);
-            }else{
-                itens.remove(itemIdx);
-            }
-            return true;
-        }
-        catch (Exception itemNaoEncontrado){
+    public void removeItemPedido(ItemPedido itemPedidoRemovido) {
+        final var shake = itemPedidoRemovido.getShake();
+        final var itemExistente = itens.get(shake);
+
+        if (itemExistente == null){
             throw new ItemNaoEncontradoException();
+        }
+
+        var quantidade = itemExistente.getQuantidade();
+
+        if (quantidade > 1){
+            itemExistente.setQuantidade(quantidade - 1);
+            itens.put(shake, itemExistente);
+        }else{
+            itens.remove(shake);
         }
     }
 
